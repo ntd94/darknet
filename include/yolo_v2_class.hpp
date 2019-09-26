1a496afc514ab2011a9c0be3ff548bdfbeb6baff
Nguyen Tien Dat <a@a>
2019-09-26 10:21:17 +0700

    change bbox_t type

--- include/yolo_v2_class.hpp
+++ include/yolo_v2_class.hpp
@@ -17,12 +17,30 @@
 #endif
 #endif
 
+#include <string>
+
+class track_info_t {
+public:
+	std::string stringinfo;
+	int age;
+	float prob;
+};
+
+
 struct bbox_t {
     unsigned int x, y, w, h;       // (x,y) - top-left corner, (w, h) - width & height of bounded box
     float prob;                    // confidence - probability that the object was found correctly
     unsigned int obj_id;           // class of object - from range [0, classes-1]
     unsigned int track_id;         // tracking id for video (0 - untracked, 1 - inf - tracked object)
     unsigned int frames_counter;   // counter of frames on which the object was detected
+    bool operator > (const bbox_t& other) const
+	{
+		return (w*h > other.w*other.h);
+	}
+	track_info_t * track_info;
+	bbox_t() {
+		track_info = new track_info_t();
+	}
 };
 
 struct image_t {
